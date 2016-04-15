e927098b626e88b8b2af18bf9dadac74ec5242ac
Ivan Krutov <vania-pooh@yandex-team.ru>
2016-04-15 14:49:49 +0400

    Update prepare-source-package.sh

--- scripts/prepare-source-package.sh
+++ scripts/prepare-source-package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=$(dpkg-parsechangelog | sed -ne 's/^Version: \(.*\)\-[0-9]\{1,\}$/\1/p' | sed -ne 's/\(.*\)\(-RELEASE\)*$/\1/p')
+VERSION=$(dpkg-parsechangelog | sed -ne 's/^Version: \(.*\)\-[0-9]\{1,\}$/\1/p' | sed -e 's/\([0-9.]*\)\(-RELEASE\)*/\1/g'
 echo "Building source package for version = $VERSION..."
 scripts/clean.sh $VERSION
 scripts/build.sh $VERSION
