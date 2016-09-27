b10060e8334800a1b5be2defc865336b376b37fe
Ivan Krutov <vania-pooh@yandex-team.ru>
2016-09-27 11:56:25 +0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VERSION=$1
 mvn -Dversion="$VERSION" -DskipTests=true clean package
-mv perspective-rest/target/lib/perspective-rest-${VERSION}.war perspective-rest/target/perspective-rest.war
+mv perspective-rest/target/lib/perspective-rest-${VERSION}.jar perspective-rest/target/perspective-rest.jar
 mv perspective-storage/target/lib/perspective-storage-${VERSION}.jar perspective-storage/target/perspective-storage.jar
 mv perspective-openstack/target/lib/perspective-openstack-${VERSION}.jar perspective-openstack/target/perspective-openstack.jar
 mv perspective-docker/target/lib/perspective-docker-${VERSION}.jar perspective-docker/target/perspective-docker.jar
